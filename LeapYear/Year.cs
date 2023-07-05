0647570326f2f54e4e47758141aae84b7ae53fa2
qmafog <qmafog6@gmail.com>
2023-07-05 03:05:17 +0300

    done

--- LeapYear/Year.cs
+++ LeapYear/Year.cs
@@ -9,9 +9,9 @@
         /// <returns>True if a specified <paramref name="year"/> is a leap year; otherwise false.</returns>
         public static bool IsLeapYear(int year)
         {
-            if (year % 4 == 0)
+            if (year % 4 == 0 && (year % 100 != 0 || (year % 100 == 0 && year % 400 == 0)))
             {
-                return true;
+                    return true;
             }
 
             return false;
